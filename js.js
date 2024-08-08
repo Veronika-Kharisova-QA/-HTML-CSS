f2d648a98732bd80ce1e0d82933746b22fe5bc88
Arsentiy <vavilov.arsentiy@gmail.com>
2024-08-08 18:42:02 +0300

    Добавила !

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Если долго мучиться, что-нибудь получится')
+	alert('Если долго мучиться, что-нибудь получится!')
 })
